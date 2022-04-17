2f179a923fcea8aa5510276fa974fe89ffbc5d26
Marco <marco@m2crowd.com>
2022-04-16 19:03:32 -0500

    add new user back and fron

--- api-connection/conn.js
+++ api-connection/conn.js
@@ -29,10 +29,37 @@ app.get('/get-data', (req, res) => {
   res.send(dataMock)
   // const query = 'SELECT * FROM users WHERE email= "' + req.query.email + '"'
   // connection.query(query, function (err, rows, fields) {
-  //   // if (err) throw err
+  //   if (err) res.send([])
 
   //   res.send(rows)
   //   console.log(rows)
+  // })
+})
+
+app.get('/register-user', (req, res) => {
+  console.log('************')
+  console.log(req.query)
+  console.log('************')
+  const dataMock = '1'
+  console.log(dataMock)
+  console.log('************')
+  res.send(dataMock)
+
+  // let values= '('
+  // let data= '('
+  // Object.entries(req.query).forEach(entry => {
+  //   const [key, value] = entry;
+  //   data = data + key + ','
+  //   values = values + '"' + value + '",'
+  // });
+  // const data_query = data.substring(0, data.length - 1) + ')'
+  // const values_query = values.substring(0, values.length - 1) + ')'
+  
+  // const query = 'INSERT INTO users ' + data_query + ' VALUES ' + values_query
+  // connection.query(query, function (err, rows, fields) {
+  //   if (err) res.status(400).send('0')
+  //   res.status(200).send(rows.affectedRows.toString())
+    
   // })
 })
 
--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from crypt import methods
 import json
 import requests
-from flask import Flask, render_template, request, session, redirect
+from flask import (
+    Flask,
+    render_template,
+    request,
+    session,
+    redirect,
+    url_for
+)
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'PruebaMarcoGomez'
@@ -28,12 +34,19 @@ def login():
             session['pass'] = data_user['password']
             session['data'] = data_user
             # Redirect to home page
-            return render_template('index.html', user=data_user)
+            return redirect(url_for('home'))
             
         else:
             msg = 'Email/Contraseña incorrecta!\n😔'
 
     return render_template('login.html', msg=msg)
+
+@app.route('/home')
+def home():  
+    if session['loggedin']:
+        return render_template('index.html', user=session['data'])
+    else:
+        return render_template('login.html', msg='🛑 No olvides iniciar sesión ⛔')
 
 @app.route('/data-user', methods=['GET', 'POST'])
 def get_data_user():
@@ -45,6 +58,32 @@ def get_data_user():
 @app.route('/add-user', methods=['GET', 'POST'])
 def add_user():
     if session['loggedin']:
-        return render_template('new-user.html', msg='probando weee')
+        return render_template('new-user.html', msg='')
     else:
         return render_template('login.html', msg='🛑 No olvides iniciar sesión ⛔')
+
+@app.route('/update-user', methods=['GET', 'POST'])
+def update_user():
+    if session['loggedin']:
+        return render_template('update-user.html', user=session['data'])
+    else:
+        return render_template('login.html', msg='🛑 No olvides iniciar sesión ⛔')
+
+@app.route('/register-user', methods=['GET', 'POST'])
+def register_user():
+    if (request.method == 'POST'
+        and 'name' in request.form
+        and 'email' in request.form
+        and 'type_user' in request.form
+        and 'password' in request.form
+    ):
+        # Create variables for easy access
+        data_to_send = '?'
+        for element in request.form:
+            data_to_send = f'{data_to_send}{element}={request.form[element]}&'
+        url = f'http://127.0.0.1:4000/register-user{data_to_send[:-1]}'
+        res = requests.get(url)
+        response = json.loads(res.text)
+        if int(response) > 0:
+            return render_template('new-user.html', msg='✅ El usuario se registró con éxito!. ✅')
+        return render_template('new-user.html', msg='⛔ Hubo un error, intente de nuevo. ⛔')
